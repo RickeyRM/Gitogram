43691f388e22475e0e318bf7852ad7d0ba02495e
RickeyRM <97_roman_97@mail.ru>
2021-07-19 18:35:38 +0300

    toggle

---
+++ src/components/posts/posts.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="posts">
+    <ul class="c-posts">
+      <li class="c-posts__item">
+        <div class="posts__img">
+          <img :src="avatar" alt="beb">
+        </div>
+        <span class="posts__username">{{ username }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    avatar: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+
+</script>
--- src/pages/feeds/feeds.vue
+++ src/pages/feeds/feeds.vue
@@ -19,25 +19,30 @@
         <template #content>
           <ul class="stories">
             <li class="stories__item" v-for="story in stories" :key="story.id">
-              <story-user-item :avatar="story.avatar" :username="story.name"/>
+              <story-user-item :avatar="story.avatar" :username="story.username"/>
             </li>
           </ul>
         </template>
       </topline>
+      <template>
+        <posts />
+      </template>
     </div>
 </template>
 <script>
 import { topline } from '../../components/topline'
 import { storyUserItem } from '../../components/storyUserItem'
 import { icon } from '../../icons'
-import { stories } from './data.json'
+import { posts } from '../../components/posts'
+import stories from './data.json'
 
 export default {
   name: 'feeds',
   components: {
     topline,
     icon,
-    storyUserItem
+    storyUserItem,
+    posts
   },
   data () {
     return {
@@ -47,18 +52,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-  .icon{
-    width: 24px;
-    color: #262626;
-
-  }
-  .header{
-    display: flex;
-    justify-content: space-between;
-  }
-  .navigation{
-    display: flex;
-    justify-content: space-between;
-  }
-</style>
+<style src="./feeds.scss" lang="scss" scoped></style>
